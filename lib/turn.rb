de525230049ea775397a183cd5ef010f9c1e965a
Yair Taylor <yairtaylor@gmail.com>
2019-04-14 15:05:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ def position_taken?(board,index)
 end
 
 def turn
-  puts "Please enter 1-9."
+  puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move? == FALSE
